d6236ec3e3dafc09795fa5f45d28e33041bde35a
Peter Mai <peter2584pm@gmail.com>
2018-03-04 21:37:05 -0800

    Initial Commit

---
+++ BumperCarSimulation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BumperCarSimulation
+//
+//  Created by Peter Mai on 3/4/18.
+//  Copyright © 2018 William Ma & Peter Mai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
